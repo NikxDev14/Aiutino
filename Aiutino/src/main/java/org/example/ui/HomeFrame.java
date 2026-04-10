cd15d6a8dd144ebf1a5d11b53de0f132a3928e6f
Nicolò Frigati <frigati.nicolo.22@itisriva.edu.it>
2026-04-10 13:48:44 +0200

    Creazione struttura home
    Implementate dipendenze varie (JavaFx e Jakarta mail) e creazione iniziale della struttura della pagina home alla quale manca il CSS e il metodo per inserire le recensioni nella home.

---
+++ Aiutino/src/main/java/org/example/ui/HomeFrame.java
@@ -0,0 +1,56 @@
+package org.example.ui;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class HomeFrame extends BorderPane {
+
+    public HomeFrame(){
+        //Applicare una classe CSS al contenitore principale
+        this.getStyleClass().add("contenitore-principale");
+
+        //Aggiungere header e body creati
+    }
+
+    //Crea la barra in cima al programma
+    private HBox creaHeader(){
+        HBox header = new HBox(20);
+        header.getStyleClass().add("header"); //Assegna una classe
+
+        Label titolo = new Label("Aiutino?");
+        titolo.getStyleClass().add("titolo");
+
+        Button btnFiltro = new Button("Filtra categorie");
+        btnFiltro.getStyleClass().add("btnFiltro");
+
+        Button btnProfilo = new Button("Profilo");
+        btnProfilo.getStyleClass().add("btnProfilo");
+
+        Region spazio = new Region(); //Componente grafico vuoto che occupa spazio
+        HBox.setHgrow(spazio, Priority.ALWAYS); //La sua dimensione cresce schiacciando ciò che c'è dopo
+
+        header.getChildren().addAll(titolo,btnFiltro,spazio,btnProfilo);
+        return header;
+    }
+
+    private ScrollPane creaBody(){
+        GridPane griglia = new GridPane(); //Griglia flessibile e variabile quando si aggiungono nodi
+        griglia.setHgap(20); //Distanza orizzontale tra colonne
+        griglia.setVgap(20); //Distanza verticale tra colonne
+        griglia.setPadding(new Insets(30)); //Margine tra contenuto e quadrati della griglia
+
+        //Assegnazione di una proporzione alle due colonne
+        ColumnConstraints col = new ColumnConstraints();
+        col.setPercentWidth(50);
+        griglia.getColumnConstraints().setAll(col,col);
+
+        //Popolazione body con recensioni
+
+        //Rendere griglia scorribile
+        ScrollPane sp = new ScrollPane(griglia);
+        sp.setFitToWidth(true);
+        sp.getStyleClass().add("body-scroll");
+        return sp;
+    }
+}
