d250b31a7bd9fe6a72d79669e2831e6d77ae7b85
Nicolò Frigati <frigati.nicolo.22@itisriva.edu.it>
2026-04-11 18:44:04 +0200

    Primo lancio di test e sistemazione CSS
    Co-Authored-By: Nora-333 <203662459+nora-333@users.noreply.github.com>

---
+++ Aiutino/src/main/java/org/example/Launcher.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Avvia il Main vero, ma partendo da una classe "normale"
+        Main.main(args);
+    }
+}
